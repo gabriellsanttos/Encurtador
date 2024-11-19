b29bab043cb2b4832c7b474a1aa9b69eb99e4701
gabriel gonçal <manelsantos041@gmail.com>
2024-11-19 10:59:51 -0300

    first commit

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+        'host' => 'localhost',   
+        'database' => 'encurtador', //Nome do banco de dados
+        'user' => 'root', //Usuário do banco de dados
+        'password' => 'root', //Senha do banco de dados
+    ];
